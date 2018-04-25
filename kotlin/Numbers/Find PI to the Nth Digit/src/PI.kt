14e0585afb890f7407af536ae02971e99fb156c9
= <=>
2018-04-24 22:16:44 -0300

    Added bounds checking on input.
    Using simple pair data class to contain either solution or exception

--- kotlin/Numbers/Find PI to the Nth Digit/src/PI.kt
+++ kotlin/Numbers/Find PI to the Nth Digit/src/PI.kt
@@ -1,2 +1,11 @@
+import com.sun.tools.corba.se.idl.InvalidArgument
+
 fun nth_digit(n: Int): Int =
-        (Math.PI * (Math.pow(10.0, n.toDouble()))).toInt() % 10
+        (Math.PI * (Math.pow(10.0, (n-1).toDouble()))).toInt() % 10
+
+fun safe_nth_digit(n: Int): Pair<InvalidArgument?, Int?> =
+        if(n>16) {
+            Pair(InvalidArgument("Digit $n higher than supported 16"), null)
+        } else {
+            Pair(null, nth_digit(n))
+        }
--- kotlin/Numbers/Find PI to the Nth Digit/test/PITest.kt
+++ kotlin/Numbers/Find PI to the Nth Digit/test/PITest.kt
@@ -12,14 +12,20 @@ data class PIPair(val pos: Int, val value: Int)
 
 fun nth_digit_test(){
     val test_cases = listOf(
-            PIPair(0, 3),
-            PIPair(1, 1),
-            PIPair(2, 4),
-            PIPair(3, 1),
-            PIPair(4, 5),
-            PIPair(5, 9))
+            PIPair(1, 3),
+            PIPair(2, 1),
+            PIPair(3, 4),
+            PIPair(4, 1),
+            PIPair(5, 5),
+            PIPair(6, 9))
     test_cases.forEach{
         assert("it.value: ${it.value} == nth_digit(it.pos): ${nth_digit(it.pos)}",
                 it.value == nth_digit(it.pos))
     }
+    test_cases.forEach{
+        assert("it.value: ${it.value} == nth_digit(it.pos): ${safe_nth_digit(it.pos).second}",
+                it.value == safe_nth_digit(it.pos).second)
+    }
+    assert("it.value: 17 == nth_digit(it.pos): ${safe_nth_digit(17).second}",
+            17 == safe_nth_digit(17).second)
 }
